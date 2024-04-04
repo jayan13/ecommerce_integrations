62f3c30fea88fe9558fc71762acb585d9b826851
Ubuntu <ubuntu@ip-172-31-12-250.ap-south-1.compute.internal>
2024-04-04 08:32:10 +0000

    report

---
+++ ecommerce_integrations/shopify/report/shopify_item_wise_sales_register/shopify_item_wise_sales_register.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Shopify Item-wise Sales Register"] = {
+	"filters": [
+
+	]
+};
